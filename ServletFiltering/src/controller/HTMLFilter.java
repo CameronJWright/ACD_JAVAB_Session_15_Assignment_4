7bc6af34eb608c61426639aa55fa360652883037
CameronJWright <53836742+CameronJWright@users.noreply.github.com>
2019-08-29 17:32:26 -0400

    ServletFiltering

---
+++ ServletFiltering/src/controller/HTMLFilter.java
@@ -0,0 +1,56 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class HTMLFilter
+ */
+@WebFilter("/HTMLFilter")
+public class HTMLFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public HTMLFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		System.out.println("Have a good day!");
+		
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
